3e891e9d7848bff2c6da59c02c4c7c200642792b
Elia Rigo <elia.rigo@gmail.com>
2012-06-28 15:57:03 +0200

    Travis Force

--- test_site_analysis.py
+++ test_site_analysis.py
@@ -49,6 +49,7 @@ class TestMyModule(unittest.TestCase):
    def test_gen_hash(self):
        from site_analysis import gen_hash
        #self.assertEqual(gen_hash('http://www.diffbot.com/',dict(p1='p1', p2='p2')),'1905959970210950507')
+       # insetrt new hash
        self.assertEqual(gen_hash('http://www.diffbot.com/',dict(p1='p1', p2='p2')),'1534481045')
 
    def test_is_valid(self):
