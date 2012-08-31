1bf9ff83780b87915e766e5b474197782aea0a7f
Davide Setti <davide.setti@gmail.com>
2012-08-31 14:02:03 +0200

    prefer lxml.etree over cElementTree

--- site_analysis.py
+++ site_analysis.py
@@ -2,7 +2,7 @@ from itertools import chain
 import re
 import os
 import logging
-from xml.etree import cElementTree as etree
+from lxml import etree
 from collections import defaultdict, OrderedDict, deque
 from optparse import OptionParser
 from urlparse import urlparse
